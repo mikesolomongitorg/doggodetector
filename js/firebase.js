2625bb99514e40498abf69c0ab38db489fbb74f7
Mike Solomon <msolomon@thrillist.com>
2018-05-02 22:25:26 -0400

    install firebase

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCPNCg2NsVUoJehBSel9xrsUlcjiwx-QE4",
+  authDomain: "doggo-detector.firebaseapp.com",
+  databaseURL: "https://doggo-detector.firebaseio.com",
+  projectId: "doggo-detector",
+  storageBucket: "",
+  messagingSenderId: "1091079895035"
+};
+firebase.initializeApp(config);
